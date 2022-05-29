5714e77d99c94846906845bebb86481a6c290c5a
Dave <db001@users.noreply.github.com>
2022-05-29 19:17:48 +0100

    Init commit

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList } from "react-native";
+import React, { useState } from "react";
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	function addGoalHandler(enteredGoalText) {
+		setCourseGoals((currGoals) => [...currGoals, { text: enteredGoalText, id: Math.random().toString() }]);
+	}
+
+	function removeGoalHandler(id) {
+		setCourseGoals((currGoals) => {
+			return currGoals.filter((goal) => goal.id !== id);
+		});
+	}
+
+	return (
+		<View style={styles.appContainer}>
+			<GoalInput onAddGoal={addGoalHandler} />
+			<View style={styles.goalsContainer}>
+				<FlatList
+					data={courseGoals}
+					renderItem={(itemData) => {
+						return (
+							<GoalItem
+								text={itemData.item.text}
+								id={itemData.item.id}
+								onDeleteItem={removeGoalHandler}
+							/>
+						);
+					}}
+					alwaysBounceVertical={false}
+					keyExtractor={(item, index) => {
+						return item.id;
+					}}
+				/>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	appContainer: {
+		paddingTop: 50,
+		paddingHorizontal: 16,
+		flex: 1,
+	},
+	goalsContainer: {
+		flex: 5,
+	},
 });
